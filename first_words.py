e688dac34e876119ac8c32ad1b67819e78fd497c
Derek Wu <34558808+DerekWu1@users.noreply.github.com>
2021-09-25 20:59:23 +1000

    Great
    Mate

--- first_words.py
+++ first_words.py
@@ -34,7 +34,7 @@ filtered_sentence = []
 #    if w not in stop_words:
 #        filtered_sentence.append(w)
 
-p#rint(filtered_sentence)
+#print(filtered_sentence)
 
 # list comprehension is a better pythonic way 
 
@@ -105,12 +105,44 @@ def process_content():
             words = nltk.word_tokenize(i)
             tagged = nltk.pos_tag(words)
 
-            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP><NN>?} """
+            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?} """
             chunkParser = nltk.RegexpParser(chunkGram)
             chunked = chunkParser.parse(tagged)
+            #print(chunked)
             chunked.draw()
 
-    #except Exception as e:
-    #    print(str(e))
+    except Exception as e:
+        print(str(e))
 
 process_content()
+
+
+###############################################################
+
+######### Part of Speech Tagging and Chinking #################
+
+###############################################################
+
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
+def process_content():
+    try:
+        for i in tokenized[5:10]:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+
+            namedEnt = nltk.ne_chunk(tagged)
+
+            namedEnt.draw()
+
+    except Exception as e:
+        print(str(e))
